7ae35cbf353b5b363b77d6c75920c5cdb921bd73
Ibraheem Abdul-Malik <ibraheem4@gmail.com>
2015-07-21 19:46:45 -0700

    updates to datadog/celery log parser

--- celery/parsers/celery_log.py
+++ celery/parsers/celery_log.py
@@ -8,7 +8,10 @@
     # [2013-02-09 15:20:43,779: INFO/MainProcess] Task entity.tasks.add_love[c8411104-ee40-49e8-ab4d-af1be60f93aa] succeeded in 0.169150829315s: None
 
     # 'received' example
-    # [2013-02-09 15:20:44,261: INFO/MainProcess] Got task from broker: user.tasks.sync_follow_open_graph[aa7d0eec-5416-4d15-b36b-10f2d85375e9] eta:[2013-02-09 15:20:47.256882]
+    # [2015-07-20 18:25:59,371: INFO/MainProcess] Received task: appratings.tasks.add[6cd42812-7a9e-49d5-9bbd-1174233441cb]
+
+    # 'scheduler' example
+    # [2015-07-20 18:24:18,036: INFO/MainProcess] Scheduler: Sending due task add-every-5-seconds (appratings.tasks.add)
 
     # 'error' example
     # [2013-02-06 14:02:02,435: WARNING/MainProcess] len() on an unsliced queryset is not allowed
@@ -18,19 +21,19 @@
 from datetime import datetime
 import re
 
-def get_timestamp(date_string):
-    """ Takes "2013-02-09 15:20:43", returns "1360423243".
-    """
-    return datetime.strptime(date_string, "%Y-%m-%d %H:%M:%S").strftime("%s")
+import common
 
 def parse_celery(logging, line):
     """ Attempt to match the line to one of three regexes. If a match is found, parse
         the line and return the metric to datadog.
     """
-    success_regex = "\[(?P<time>\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}).*Task (?P<task_name>[\w\.]+)\[(?P<task_id>[\w\-]+)\] succeeded in (?P<duration>\d+\.\d+)s"
+    success_regex = "\[(?P<timestamp>\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.*?(\d+)).*Task (?P<task>[\w\.]+)\[(?P<task_id>[\w\-]+)\] succeeded in (?P<duration>\d+\.\d+)s: (?P<msg>\S+$)"
 
-    received_regex = "\[(?P<time>\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}).*Got task from broker: (?P<task_name>[\w\.]+)\[(?P<task_id>[\w\-]+)\]"
-    error_regex = "\[(?P<time>\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}).*"
+    received_regex = "\[(?P<timestamp>\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.*?(\d+)).*Received task: (?P<task>[\w\.]+)\[(?P<task_id>[\w\-]+)\]"
+
+    scheduler_regex = "\[(?P<timestamp>\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.*?(\d+)).*Scheduler: Sending due task (?P<task_name>[\w\-\.]+) \((?P<task>.*?)\)"
+
+    error_regex = "\[(?P<timestamp>\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.*?(\d+)).*"
 
     attr_dict = {
         "metric_type": "counter",
@@ -40,10 +43,11 @@ def parse_celery(logging, line):
     # Task success?
     match = re.match(success_regex, line)
     if match:
-        data = match.groupdict()
+        event = match.groupdict()
+        celery_event = "success"
         # could also return task name as an attribute?
-        return ("celery.tasks.success.%s" % data['task_name'],
-            get_timestamp(data['time']),
+        return ("celery.%s.%s" % (celery_event, event['task']),
+            common.parse_date(event['timestamp']),
             1, # metric count
             attr_dict
         )
@@ -51,9 +55,21 @@ def parse_celery(logging, line):
     # Task received?
     match = re.match(received_regex, line)
     if match:
-        data = match.groupdict()
-        return ("celery.tasks.received.%s" % data['task_name'],
-            get_timestamp(data['time']),
+        event = match.groupdict()
+        celery_event = "received"
+        return ("celery.%s.%s" % (celery_event, event['task']),
+            common.parse_date(event['timestamp']),
+            1, # metric count
+            attr_dict
+        )
+
+    # Task scheduled?
+    match = re.match(scheduler_regex, line)
+    if match:
+        event = match.groupdict()
+        celery_event = "scheduler"
+        return ("celery.%s.%s" % (celery_event, event['task']),
+            common.parse_date(event['timestamp']),
             1, # metric count
             attr_dict
         )
@@ -62,22 +78,22 @@ def parse_celery(logging, line):
     # get one line here, so we can't look at the whole traceback.
     match = re.match(error_regex, line)
     if match:
-        data = match.groupdict()
-        return ("celery.tasks.error",
-            get_timestamp(data['time']),
+        event = match.groupdict()
+        celery_event = "error"
+        return ("celery.%s" % celery_event,
+            common.parse_date(event['timestamp']),
             1, # metric count
             attr_dict
         )
 
 
 def test_success(logging):
-    """ Test task success line.
-    """
-    
+    """ Test success line.
+    """    
     success_input = "[2013-02-09 15:20:43,779: INFO/MainProcess] Task entity.tasks.add_love[c8411104-ee40-49e8-ab4d-af1be60f93aa] succeeded in 0.169150829315s: None"
     expected = (
-        "celery.tasks.success.entity.tasks.add_love",
-        "1360423243",
+        "celery.success.entity.tasks.add_love",
+        "1360452043",
         1,
         {"metric_type": "counter",
          "unit": "request"}
@@ -87,12 +103,26 @@ def test_success(logging):
 
 
 def test_received(logging):
-    """ Test task receive line.
+    """ Test received line.
     """
-    received_input = "[2013-02-09 15:20:44,261: INFO/MainProcess] Got task from broker: user.tasks.sync_follow_open_graph[aa7d0eec-5416-4d15-b36b-10f2d85375e9] eta:[2013-02-09 15:20:47.256882]"
+    received_input = "[2015-07-20 18:25:59,371: INFO/MainProcess] Received task: appratings.tasks.add[6cd42812-7a9e-49d5-9bbd-1174233441cb]"
     expected = (
-        "celery.tasks.received.user.tasks.sync_follow_open_graph",
-        "1360423244",
+        "celery.received.appratings.tasks.add",
+        "1437441959",
+        1,
+        {"metric_type": "counter",
+         "unit": "request"}
+    )
+    actual = parse_celery(logging, received_input)
+    assert expected == actual, "%s != %s" % (expected, actual)
+
+def test_scheduler(logging):
+    """ Test scheduler line.
+    """
+    received_input = "[2015-07-20 18:24:18,036: INFO/MainProcess] Scheduler: Sending due task add-every-5-seconds (appratings.tasks.add)"
+    expected = (
+        "celery.scheduler.appratings.tasks.add",
+        "1437441858",
         1,
         {"metric_type": "counter",
          "unit": "request"}
@@ -106,8 +136,8 @@ def test_error(logging):
     """
     error_input = "[2013-02-06 14:02:02,435: WARNING/MainProcess] len() on an unsliced queryset is not allowed"
     expected = (
-        "celery.tasks.error",
-        "1360159322",
+        "celery.error",
+        "1360188122",
         1,
         {"metric_type": "counter",
          "unit": "request"}
@@ -121,8 +151,13 @@ def test():
     logging.basicConfig(level=logging.DEBUG)
 
     test_success(logging)
+    print "success: passed"
     test_received(logging)
+    print "received: passed"
+    test_scheduler(logging)
+    print "scheduler: passed"
     test_error(logging)
+    print "error: passed"
 
 
 if __name__ == '__main__':
